c155d10590c2a476f172a697a1500116e05e20c7
MuhammedHassan <Moh.alkhuzaee@gmail.com>
2023-08-31 03:27:20 +0300

    Working on Home page Almost Done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Typed from 'typed.js';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+ 
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const typingEffect = new Typed('.multiText', {
+      strings: ['Judge', 'Lawyer', 'Mizan AI', 'Assistant Tool'],
+      loop: true,
+      typeSpeed : 100,
+      backSpeed: 80,
+      backDelay: 150
+    });
+  }
 }
+
+
