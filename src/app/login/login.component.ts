4a3af7491e82e84016350354c7d80ea6d01d5293
MuhammedHassan <Moh.alkhuzaee@gmail.com>
2023-09-13 18:03:50 +0300

    Finishing Login Page with CSS

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { iconsData } from './LoginData';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,45 @@ import { iconsData } from './LoginData';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  iconsData = iconsData;
-  signedUpUsers: any[] = [];
-  singupObj = [];
+  currentLang: string;
+loopArray = new Array(342);
+  constructor(public translate: TranslateService,private fb: FormBuilder) {
+    this.currentLang = translate.currentLang;
+  }
+  hide = true;
+
+  ngOnInit(): void {
+    this.toggleLang();
+    this.createForm();
+  }
+
+  toggleLang() {
+    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en'; // Toggle between 'en' and 'ar' or your language codes
+    this.translate.use(this.currentLang);
+  }
+  registrationForm: FormGroup = new FormGroup({});
+
+
+  createForm() {
+    this.registrationForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.pattern(/^[A-Za-z_][A-Za-z0-9_]*$/)
+      ]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  
+
+  submitForm() {
+    if (this.registrationForm.valid) {
+      console.log('Form submitted with data:', this.registrationForm.value);
+      this.registrationForm.reset({
+        username: '',
+        password: ''
+      });
+    }
+  }
 }
+
+
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,19 +1,6 @@
 import {Component} from '@angular/core';
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
-import {BreakpointObserver} from '@angular/cdk/layout';
-import {StepperOrientation, MatStepperModule} from '@angular/material/stepper';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {NgSwitch, NgSwitchCase, AsyncPipe, CommonModule} from '@angular/common';
-import { SignupData } from './signupData';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-
-
 
 @Component({
   selector: 'app-signup',
@@ -22,16 +9,12 @@ import { TranslateService } from '@ngx-translate/core';
 })
 
 export class SignupComponent {
-  hide = true;
-
-onClick() {
-console.log("Clicked");
-
-}
+  loopArray = new Array(342);
 currentLang: string;
   constructor(public translate: TranslateService,private fb: FormBuilder) {
     this.currentLang = translate.currentLang;
   }
+  hide = true;
 
   ngOnInit(): void {
     this.toggleLang();
@@ -42,10 +25,6 @@ currentLang: string;
     this.currentLang = this.currentLang === 'en' ? 'ar' : 'en'; // Toggle between 'en' and 'ar' or your language codes
     this.translate.use(this.currentLang);
   }
-
-  IconsData = SignupData;
-  login = 'login'
-
   registrationForm: FormGroup = new FormGroup({});
 
 
